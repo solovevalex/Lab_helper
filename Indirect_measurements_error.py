0046b24314211255e48b584f284a2312f123ebd8
Soneyka <bogakovskaia.siu@phystech.edu>
2021-12-11 14:43:17 +0300

    Получена функция, вычисляющая значения выражения. Поправлены другие мелкие нюансы.
    В работе:
    1) Иной способ обозначения погрешностей
    2) Формирование словаря на основе введенных пользователем данных

--- Indirect_measurements_error.py
+++ Indirect_measurements_error.py
@@ -5,13 +5,36 @@ from sympy.parsing.sympy_parser import standard_transformations, implicit_multip
 transformations = (standard_transformations + (implicit_multiplication_application,))
 
 '''
-- Относительная погрешность
-- Latex'овский синтаксис
+Получена формула для косвенной погрешности на основании введенной строки-формулы. Требуется добавить:
+1) Относительная погрешность
+2) Latex'овский синтаксис
++ 3) Обернуть введенную формулу в функцию
++ 4) Обернуть полученную формулу для косвенной погрешности в функцию
+5) Добавить check на ошибки в веденной формуле
+'''
+
+
+'''
++1) Парсим функцию
++2) Находим все "символы"
++3) Получаем список констант
++4) Получаем список переменных
+- нужен способ обозначения погрешностей в формуле 5) Находим функцию ошибки в виде строки
++6) Парсим ее
+- не получится 7) Модифицируем ее
+8) Как-то получаем словарь со всеми-всеми значениями
++9) На его основе получаем значения функции и функции ошибки
+'''
+
+'''
+func_val считает значение переданной функции.
+function -- функция;
+values -- словарик значений.
 '''
 
 '''Передаем функцию в виде строки'''
 #str_func = "sin(x) * cos(y) * tan(z)"
-str_func = "x / y"
+str_func = "x * y"
 
 '''Передаем список констант'''
 constants = []
@@ -40,15 +63,25 @@ for i in range(len(func_variables)):
 str_var_only = [x for x in str_func_variables if x not in constants]
 
 
-diff_func = ""
+str_diff_func = ""
 '''Создаем строку с итоговые дифференциалом'''
 for i in range(len(str_var_only)):
     #Временно значения дифференциалов обозначила как d'значение переменной'
-    diff ="(" + str(sym.diff(func, str_to_var[str_var_only[i]])) + "* d" + str_var_only[i] + ")**2"
-    diff_func += diff
+    diff ="(" + str(sym.diff(func, str_to_var[str_var_only[i]])) + " * d" + str_var_only[i] + ")**2"
+    str_diff_func += diff
     if i < len(str_var_only)-1:
-        diff_func += " + "
+        str_diff_func += " + "
 
-diff_func = "sqrt(" + diff_func + ")"
+str_error_func = "sqrt(" + str_diff_func + ")"
 print("Выражение для подсчета косвенной погрешности заданной величины:")
-print(diff_func)
+print(str_error_func)
+
+error_func = parse_expr(str_error_func, transformations=transformations)
+
+#плохо...
+print(str(error_func))
+
+
+def func_val(function, values):
+    value = function.subs(values).evalf()
+    return value
