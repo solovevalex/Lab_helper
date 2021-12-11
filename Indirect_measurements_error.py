d85630a402662533665a00d6345af8bb25d18da1
Soneyka <bogakovskaia.siu@phystech.edu>
2021-12-11 16:33:18 +0300

    Найден способ отображения погрешностей.
    Предстоит:
    1) До конца разобраться с получением значений
    2) Добавить check

--- Indirect_measurements_error.py
+++ Indirect_measurements_error.py
@@ -13,25 +13,6 @@ transformations = (standard_transformations + (implicit_multiplication_applicati
 5) Добавить check на ошибки в веденной формуле
 '''
 
-
-'''
-+1) Парсим функцию
-+2) Находим все "символы"
-+3) Получаем список констант
-+4) Получаем список переменных
-- нужен способ обозначения погрешностей в формуле 5) Находим функцию ошибки в виде строки
-+6) Парсим ее
-- не получится 7) Модифицируем ее
-8) Как-то получаем словарь со всеми-всеми значениями
-+9) На его основе получаем значения функции и функции ошибки
-'''
-
-'''
-func_val считает значение переданной функции.
-function -- функция;
-values -- словарик значений.
-'''
-
 '''Передаем функцию в виде строки'''
 #str_func = "sin(x) * cos(y) * tan(z)"
 str_func = "x * y"
@@ -63,25 +44,43 @@ for i in range(len(func_variables)):
 str_var_only = [x for x in str_func_variables if x not in constants]
 
 
-str_diff_func = ""
+str_diff_func_user = ""
+str_diff_func_calc = ""
+diff_list = []
 '''Создаем строку с итоговые дифференциалом'''
 for i in range(len(str_var_only)):
     #Временно значения дифференциалов обозначила как d'значение переменной'
-    diff ="(" + str(sym.diff(func, str_to_var[str_var_only[i]])) + " * d" + str_var_only[i] + ")**2"
-    str_diff_func += diff
+    diff_user ="(" + str(sym.diff(func, str_to_var[str_var_only[i]])) + " * d" + str_var_only[i] + ")**2"
+    diff_list.append("d" + str_var_only[i])
+    diff_calc ="(" + str(sym.diff(func, str_to_var[str_var_only[i]])) + str_var_only[i].upper() + ")**2"
+    str_diff_func_user += diff_user
+    str_diff_func_calc += diff_calc
     if i < len(str_var_only)-1:
-        str_diff_func += " + "
+        str_diff_func_user += " + "
+        str_diff_func_calc += " + "
 
-str_error_func = "sqrt(" + str_diff_func + ")"
+str_error_func_user = "sqrt(" + str_diff_func_user + ")"
+str_error_func_calc = "sqrt(" + str_diff_func_calc + ")"
 print("Выражение для подсчета косвенной погрешности заданной величины:")
-print(str_error_func)
+print(str_error_func_user)
 
-error_func = parse_expr(str_error_func, transformations=transformations)
-
-#плохо...
-print(str(error_func))
+error_func = parse_expr(str_error_func_calc, transformations=transformations)
 
 
+
+'''!!! Для Саши'''
+'''constants -- список констант (совпадает с тем, что ты передаешь мне в начале)'''
+print(constants)
+'''str_var_only -- список средних значений переменных'''
+print(str_var_only)
+'''diff_list = список дифференциалов'''
+print(diff_list)
+
+'''
+func_val считает значение переданного выражения.
+function -- функция;
+values -- словарик значений.
+'''
 def func_val(function, values):
     value = function.subs(values).evalf()
     return value
