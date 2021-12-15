f724117f36995ae2c1990cc21a530764664852b3
Soneyka <bogakovskaia.siu@phystech.edu>
2021-12-15 14:20:07 +0300

    1) Увеличила возможности программы до использования букв старшего регистра. 2) Теперь get_error_func получает формулу для относительной погрешности величины.

--- Indirect_measurements_error.py
+++ Indirect_measurements_error.py
@@ -26,7 +26,8 @@ def exp_value(str_exp, values):
     return value
 
 
-'''get_error_func -- на основе выражения-строки и списка констант считает формулу для косвенной погрешности измерения.
+'''
+get_error_func -- на основе выражения-строки и списка констант считает формулу для косвенной погрешности измерения.
 Передаются значения:
     str_exp -- введенное выражение;
     constants -- список констант.
@@ -52,10 +53,16 @@ def get_error_func(str_exp, constants):
     diff_list_user = []
     diff_list_calc = []
     for i in range(len(str_var_only)):
-        diff_user = "(" + str(sym.diff(exp, str_to_var[str_var_only[i]])) + " * d" + str_var_only[i] + ")**2"
+        expr = sym.diff(exp, str_to_var[str_var_only[i]])
+        expr = sym.simplify(expr / exp)
+        diff_user = "(" + str(expr) + " * d" + str_var_only[i] + ")**2"
         diff_list_user.append("d" + str_var_only[i])
-        diff_calc = "(" + str(sym.diff(exp, str_to_var[str_var_only[i]]))+ " * " + str_var_only[i].upper() + ")**2"
-        diff_list_calc.append(str_var_only[i].upper())
+        if str_var_only[i].isupper() == True:
+            diff_calc = "(" + str(expr) + " * " + str_var_only[i].lower() + ")**2"
+            diff_list_calc.append(str_var_only[i].lower())
+        else:
+            diff_calc = "(" + str(expr)+ " * " + str_var_only[i].upper() + ")**2"
+            diff_list_calc.append(str_var_only[i].upper())
         str_diff_func_user += diff_user
         str_diff_func_calc += diff_calc
         if i < len(str_var_only) - 1:
@@ -66,10 +73,13 @@ def get_error_func(str_exp, constants):
     return str_error_func_user, str_var_only, diff_list_user, str_error_func_calc, diff_list_calc
 
 '''
-Всякие проверки
-
-my_str = "x * y"
+my_str = "x * Y"
 str_error_func_user, str_var_only, diff_list_user, str_error_func_calc, diff_list_calc = get_error_func(my_str,[])
 print(str_error_func_calc)
-print(exp_value(my_str, {'x':5, 'y':4}))
+
+print(exp_value(str_error_func_calc, {'x':5, 'y':4, 'X':0.1, 'Y':0.2}))
+
+
+print(diff_list_user)
+print(diff_list_calc)
 '''
