d53b351a0ad93a2c64f6df976608693add5dd905
Bernakevich Anna <bernakevich.as@phystech.edu>
2021-12-16 21:12:05 +0300

    Добавлен параметр coeffs_1 в функцию plotting

--- Function_Plotting_Graph.py
+++ Function_Plotting_Graph.py
@@ -17,27 +17,24 @@ def plotting_graph(formulas: list, x: list, y: list, title: str,
     :param extr: нужны ли экстремумы функции
     :return: check: bool (check == True, если график можно построить, иначе check == False)
     '''
-    try:
-        check_1 = check_types(formulas, x, y, error, roots, extr)
-        formulas_1, coeffs_1 = parse(formulas)
-        xdata, ydata, check_3 = data_preparing(x, y)
-        popt, pcov, check_2 = approximation(xdata, ydata, coeffs_1)
-        if check_1 and check_2 and check_3:
-            plotting(formulas, xdata, ydata, popt, pcov, title, x_label, y_label, error, roots, extr)
-            return True
-        else:
-            return False
-    except BaseException:
+    check_1 = check_types(formulas, x, y, error, roots, extr)
+    formulas_1, coeffs_1 = parse(formulas)
+    xdata, ydata, check_3 = data_preparing(x, y)
+    popt, pcov, check_2 = approximation(xdata, ydata, coeffs_1)
+    if check_1 and check_2 and check_3:
+        plotting(formulas, coeffs_1, xdata, ydata, popt, pcov, title, x_label, y_label, error, roots, extr)
+        return True
+    else:
         return False
 
 # пример входных данных
-# formulas = ['a \cdot x^3 + b \cdot x^2' , 'a*x + b']
-# x = ['-5 -4 -3 -2 -1 0 1 2 3 4 5', '-5 -4 -3 -2 ']
-# y = ['0 16 18 12 4 0.3 6 28 72 144 250', '-6 -5 -4 -3']
-# title = 'График_1'
-# x_label = 't, c'
-# y_label = 'l, m'
-# error = True
-# roots = True
-# extr = True
-# check = plotting_graph(formulas, x, y, title, x_label, y_label, error, roots, extr)
+formulas = ['a \cdot x^3 + b \cdot x^2', 'a*x**2 + b', 'a*x + b']
+x = ['-5 -4 -3 -2 -1 0 1 2 3 4 5', '-5 -4 -3 -2 -1 0 1 2 3 4 5', '1 2 3 4 5']
+y = ['0 16 18 12 4 0.3 6 28 72 144 250', '375 240 135 60,1 15 0 15 60 135 240 375', '11 12 13 14 15']
+title = 'График_1'
+x_label = 't, c'
+y_label = 'l, m'
+error = True
+roots = True
+extr = True
+check = plotting_graph(formulas, x, y, title, x_label, y_label, error, roots, extr)
